7f6a8a62d131a588ee5dc6ff47a15983dc63e3ef
Marco Antonio Manrique Acha <63822742+MarcoMnrq@users.noreply.github.com>
2022-09-30 23:53:17 -0500

    chore: added tailwind support

--- popup.vue
+++ popup.vue
@@ -1,5 +1,47 @@
 <template>
   <div>
+    <header class="text-gray-600 body-font">
+      <div
+        class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a
+          class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            viewBox="0 0 24 24">
+            <path
+              d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span class="ml-3 text-xl">Tailblocks</span>
+        </a>
+        <nav
+          class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          <a class="mr-5 hover:text-gray-900">First Link</a>
+          <a class="mr-5 hover:text-gray-900">Second Link</a>
+          <a class="mr-5 hover:text-gray-900">Third Link</a>
+          <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+        </nav>
+        <button
+          class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          Button
+          <svg
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            class="w-4 h-4 ml-1"
+            viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </header>
     <h2 class="text-center">
       Welcome to your
       <a href="https://www.plasmo.com" target="_blank">Plasmo</a> Extension!
@@ -19,23 +61,6 @@
   <a href="https://docs.plasmo.com" target="_blank"> View Docs </a>
 </template>
 
-<style>
-.container {
-  min-width: 470px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 47px;
-}
-.text-center {
-  text-align: center;
-}
-.action {
-  color: #470;
-  font-weight: bold;
-}
-</style>
-
 <script setup>
 import { reactive } from "vue"
 
@@ -51,3 +76,7 @@ function decrement() {
   state.action = "decrement"
 }
 </script>
+
+<style scoped>
+@import './main.css';
+</style>
